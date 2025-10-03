a5e1a65697614bb7aee6626bfa2fa8352791a118
Ziggy <Bladetrain3r@gmail.com>
2025-10-03 13:04:40 +0200

    CLEANUP: Migrated docs and extras to extras repo

--- DOCS/vol2/Analyse.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cat *.md > blob
-echo "Turtles: $(grep -i "turtle" blob | wc -l)
-Pipes: $(grep "pipe" blob | wc -l)
-Tools: $(grep "tool" blob | wc -l)" | python3 MLBarchart.py 
-rm blob
